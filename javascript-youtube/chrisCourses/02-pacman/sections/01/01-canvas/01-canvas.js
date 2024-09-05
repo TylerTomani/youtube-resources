a799b5e0ea7c61f5b31c6de626a9bc3fcd58335d
Tyler Tomani <ttomani@gmail.com>
2024-09-05 18:42:33 -0400

    main

---
+++ javascript-youtube/chrisCourses/02-pacman/sections/01/01-canvas/01-canvas.js
@@ -0,0 +1,11 @@
+export function drawMove(){
+
+    
+    const canvas = document.querySelector('canvas')
+    const c = canvas.getContext('2d')
+    console.log(canvas)
+    c.fillStyle = 'black'
+    c.fill(0,0,innerWidth,innerWidth)
+    console.log('lkjd')
+}
+// drawMove()
--- javascript-youtube/chrisCourses/js/handlePage.js
+++ javascript-youtube/chrisCourses/js/handlePage.js
@@ -31,7 +31,6 @@ addEventListener('DOMContentLoaded', () =>{
     // displayPartTitle((iScript  -1))    
 })
 // ?///////////////////////////////////
-
 const keys = {
     shift :{
         pressed: false
@@ -96,7 +95,7 @@ addEventListener('keydown', e => {
 
     if(!isNaN(letter)){
         let intLetter = parseInt(letter)
-        if(intLetter <= arrScripts.length){
+            if(intLetter <= arrScripts.length){
 
             htmlScript = arrScripts[intLetter]
             htmlScript = `./scripts-html/${htmlScript}`
@@ -105,7 +104,6 @@ addEventListener('keydown', e => {
         }
     }
 })
-
 nxtBtn.addEventListener('click', e => {
     iScript = (iScript + 1) % arrScripts.length
     htmlScript = arrScripts[iScript]
@@ -120,27 +118,7 @@ prevBtn.addEventListener('click', e => {
     loadScript(htmlScript)
     displayPartTitle(iScript)
 })
-function loadScript(injectScript) {
-    // Fetch the JavaScript file content
-    fetch(injectScript)
-        .then(response => response.text())
-        .then(data => {
-            parentCopyCode.innerHTML = data;
-            // Extract script content
-            const tempDiv = document.createElement('div');
-            tempDiv.innerHTML = data;
-            const scriptContent = parentCopyCode.textContent;            
-            // Remove old script elements if they exist
 
-            // Create and append new script element
-            const newScriptElement = document.createElement('script');
-            newScriptElement.type = 'text/javascript';
-            newScriptElement.textContent = scriptContent;
-            newScriptElement.setAttribute('data-dynamic', 'true'); // Optional: mark as dynamic to easily remove later
-            document.body.appendChild(newScriptElement);
-        })
-        .catch(error => console.error('Error loading script:', error));
-}
 function displayPartTitle(iScript){
     iScript += 1
     footer.innerHTML = ``
@@ -259,4 +237,25 @@ function displayPartTitle(iScript){
             removePopup()
             break
     }
+}
+function loadScript(injectScript) {
+    // Fetch the JavaScript file content
+    fetch(injectScript)
+        .then(response => response.text())
+        .then(data => {
+            parentCopyCode.innerHTML = data;
+            // Extract script content
+            const tempDiv = document.createElement('div');
+            tempDiv.innerHTML = data;
+            const scriptContent = parentCopyCode.textContent;
+            // Remove old script elements if they exist
+
+            // Create and append new script element
+            const newScriptElement = document.createElement('script');
+            newScriptElement.type = 'text/javascript';
+            newScriptElement.textContent = scriptContent;
+            newScriptElement.setAttribute('data-dynamic', 'true'); // Optional: mark as dynamic to easily remove later
+            document.body.appendChild(newScriptElement);
+        })
+        .catch(error => console.error('Error loading script:', error));
 }
