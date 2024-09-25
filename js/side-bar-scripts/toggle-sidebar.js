b5538d7d064d4ff3f51cc851d6e376624ed73ebe
tt <tt@tts-MacBook-Pro.lan>
2024-09-25 02:54:11 -0400

    main

--- js/side-bar-scripts/toggle-sidebar.js
+++ js/side-bar-scripts/toggle-sidebar.js
@@ -1,13 +1,16 @@
-const sideBarBtn = document.querySelector("#sideBarBtn")
+export const sideBarBtn = document.querySelector("#sideBarBtn")
 const sideBar = document.querySelector('.side-bar')
 
 function toggleSideBar(){
     sideBar.classList.toggle('active')
 }
-sideBarBtn.onClick = function(){
-    toggleSideBar()
-}
 
+
+sideBarBtn.addEventListener('click', e => {
+    e.preventDefault()
+    toggleSideBar()
+
+})
 sideBarBtn.addEventListener('keydown', e => {
     let letter = e.key.toLowerCase() 
     if(letter == 'enter' ){
