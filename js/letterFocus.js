966c7814340880b67800e208d838e74bed617f92
Tyler Tomani <ttomani@gmail.com>
2025-05-29 06:53:44 -0400

    main

--- js/letterFocus.js
+++ js/letterFocus.js
@@ -1,10 +1,31 @@
 addEventListener('keydown', e => {
-    const allAs = document.querySelectorAll('a')
     let letter = e.key.toLowerCase()
-    let letteredAs = [...allAs].filter( a => {
+    
+    const allAs = [...document.querySelectorAll('a')].filter( a => {
         const rect = a.getBoundingClientRect()
-        console.log(a,rect.height)
+        // console.log(a,rect.height)
         return a.offsetParent != null &&  rect.width > 0 && rect.height >0
     })
+
+    const letteredAs = allAs.filter( a =>{
+        const text = a.innerText.trim().toLowerCase();
+        console.log(text)
+        return text.startsWith(letter)
+    })
+
+    if(letteredAs === 0 ) return 
+
+    const activeEl = document.activeElement
+    const iAllAs = [...allAs].indexOf(activeEl)
+    const iLetteredAs = [letteredAs].indexOf(activeEl)
+    
+    if(letter !== window.lastLetterPressed){
+        let iLetter 
+        if(e.shiftKey){
+            iLetter = iLetter
+        }      
+    }
+    
+
 
 })
