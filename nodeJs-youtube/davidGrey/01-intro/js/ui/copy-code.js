078310ad530bc044069007b20d8f14b15630b41f
tt <tt@MacBookPro.lan>
2025-08-21 14:49:59 -0400

    main

--- nodeJs-youtube/davidGrey/01-intro/js/ui/copy-code.js
+++ nodeJs-youtube/davidGrey/01-intro/js/ui/copy-code.js
@@ -17,6 +17,7 @@ export function addCopyCode(){
                 }
                 // copyTextToClipboard(e.target.innerText)
                 animate(el)
+                el
             }
         })
     })
